03152dc6ced36555acadc6196b21606dd3d1dd09
Behzad Tabibian <btabibian@gmail.com>
2012-03-08 16:08:47 +0000

    refactored classifier.
    Signed-off-by: Behzad Tabibian <btabibian@gmail.com>

--- classify.py
+++ classify.py
@@ -4,65 +4,84 @@ import os
 import mlpy
 import random
 
-    
-def learn(data, y):
+def preproccessing(data,y):
     y=numpy.array(y,dtype=numpy.int)-1
-    #y=1
     mu=numpy.mean(data,0)
+    
     data = data-numpy.tile(mu,(numpy.size(data,0),1))
     data=data/numpy.tile(numpy.std(data,0),(numpy.size(data,0),1))
+    
     randperm=random.sample(range(0,numpy.size(data,0)),numpy.size(data,0))
     data=data[randperm,:]
     y=y[randperm,:]
+    
     pca=mlpy.PCA()
     pca.learn(data)
     data_pca=pca.transform(data,k=5)
-    X=data_pca
-    idx=mlpy.cv_kfold(numpy.size(X,0), numpy.size(X,0), strat=None, seed=4)
+    return (data_pca,y)
+def train(x,y):
+    logReg=mlpy.LibLinear(solver_type='mcsvm_cs')
+    logReg.learn(x,y)
+    return logReg
+def evaluate(y_model,y_true,confusion=None):
+    if(confusion!=None):
+        confusion[y_model,y_true]=confusion[y_model,y_true]+1
+    return numpy.size(numpy.nonzero(y_model==y_true))/float(numpy.size(y_true)),confusion
+    
+def cross_validation(data,y):
+    idx=mlpy.cv_kfold(numpy.size(data,0), numpy.size(data,0), strat=None, seed=4)
     corrects=0
     falses=0
     confusion=numpy.zeros((3,3))
-    
     testavg = 0
     testavgcount = 0
-    
+    test_res=0
     for tr,ts in idx:
-        X_tr=data_pca[tr,:]
+        X_tr=data[tr,:]
         Y_tr=y[tr]
-        X_ts=data_pca[ts,:]
+        X_ts=data[ts,:]
         Y_ts=y[ts]
         
-        logReg=mlpy.LibLinear(solver_type='mcsvm_cs')
-        logReg.learn(X_tr,Y_tr)
-        w=logReg.w()
-        y_training_pred=logReg.pred(X_tr)
+        model=train(X_tr,Y_tr)
+        y_training_pred=model.pred(X_tr)
         y_trained=y_training_pred.copy()
+        y_dat=model.pred(X_ts)    
         
-        print(numpy.size(numpy.nonzero(y_trained==Y_tr))/float(numpy.size(Y_tr)))
-        y_dat=logReg.pred(X_ts)
-        
-        testavg = testavg + numpy.size(numpy.nonzero(y_trained==Y_tr))/float(numpy.size(Y_tr))
+        acc,conf=evaluate(y_trained,Y_tr)
+        testavg = testavg + acc
+        acc,confusion=evaluate(y_dat,Y_ts,confusion)
+        test_res=test_res+acc
         testavgcount = testavgcount + 1
-        
-        corrects=corrects+numpy.size(numpy.nonzero(y_dat==Y_ts))
-        falses=falses+numpy.size(Y_ts)-numpy.size(numpy.nonzero(y_dat==Y_ts))
-        confusion[y_dat,Y_ts]=confusion[y_dat,Y_ts]+1
-        
-    print "\n\n"
-    print(testavg/testavgcount)
-    print(falses)
-    print(corrects)
-    print(corrects/float(falses+corrects))
-    print(confusion)
+              
+    return confusion, testavg/float(testavgcount), test_res/float(testavgcount)
+def learn(data, y):
     
+    (X,y)=preproccessing(data,y)
+    model = train(X,y)
+
+    confusion, training_error,test_error=cross_validation(X,y)
+    print('average training error: '+str(training_error))
+    print('test error: '+str(test_error))
+    print('confusion matrix\n'+str(confusion))
     
+def write_features(x,y):
+     write=file('features.txt','a');
+     p=(y, x)
+     for ii in range(0,numpy.size(x,0)):
+         i=x[ii,:]
+         j=y[ii]
+         for k in i:
+             write.write(str(k)+',')
+         write.write(str(j))
+         write.write('\r\n')
+     write.close()
 
 def script():
     [X,y]=read_file()
-    print(X)
-    print(y)
+    #x_dat,y=preproccessing(X,y)
+    #write_features(x_dat,y)
     learn(X,y)
-	
+
 def read_file():
     read=file('output.txt','r')
     full_data=None
@@ -81,8 +100,7 @@ def read_file():
             full_data=numpy.vstack((full_data,set))
         else:
             full_data=set
-    print(numpy.size(full_data,0))
     return (full_data[:,0:-1],full_data[:,-1])
-
+script()
 
 
